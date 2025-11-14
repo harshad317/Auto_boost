7fdc0c8e001f368c1455543dfe4087e4adfaf34c
Harshad Patil <harsh.patil317.hp@gmail.com>
2025-11-14 18:04:48 +0530

    Add files via upload

--- src/auto_boost/__init__.py
+++ src/auto_boost/__init__.py
@@ -1,6 +1,6 @@
 """Auto Boost package."""
 
-from .cli import AutoBoost, build_model, build_scaler, main, parse_args
+from .cli import AutoBoost, build_model, build_scaler, infer_problem_type, main, parse_args
 
-__all__ = ["AutoBoost", "build_model", "build_scaler", "main", "parse_args"]
-__version__ = "0.1.3"
+__all__ = ["AutoBoost", "build_model", "build_scaler", "infer_problem_type", "main", "parse_args"]
+__version__ = "0.1.4"
